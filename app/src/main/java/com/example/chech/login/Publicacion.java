38c5eabd3f522598b78a25317a8241702c51b103
alexayala14 <alex.ayala.tp@gmail.com>
2018-10-02 11:44:34 -0300

    nuevo RecyclerView y eliminacion de activity login

---
+++ app/src/main/java/com/example/chech/login/Publicacion.java
@@ -0,0 +1,40 @@
+package com.example.chech.login;
+
+public class Publicacion {
+    private String nombre;
+    private String descripcion;
+    private int foto;
+
+    public Publicacion(){
+
+    }
+    public Publicacion(String nombre, String descripcion, int foto) {
+        this.nombre = nombre;
+        this.descripcion = descripcion;
+        this.foto = foto;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public int getFoto() {
+        return foto;
+    }
+
+    public void setFoto(int foto) {
+        this.foto = foto;
+    }
+}
